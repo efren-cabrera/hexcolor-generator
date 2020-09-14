7f664968c1dffffaa77e40b4d19b6cda8cf963e5
Efren Cabrera <efren@cabrera.dev>
2020-09-14 10:31:56 -0700

    Add allowed origins for CORS

---
+++ src/main/java/dev/kodice/hexcolorgenerator/config/WebMVCConfig.java
@@ -0,0 +1,13 @@
+package dev.kodice.hexcolorgenerator.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMVCConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
+	}
+
+}

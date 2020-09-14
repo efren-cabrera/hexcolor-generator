12a665bbe5ee707254125567b1f0893befb03dc6
Efren Cabrera <efren@cabrera.dev>
2020-09-14 08:47:43 -0700

    Add DTO to response

--- src/main/java/dev/kodice/hexcolorgenerator/web/HexColorGeneratorController.java
+++ src/main/java/dev/kodice/hexcolorgenerator/web/HexColorGeneratorController.java
@@ -3,11 +3,12 @@ package dev.kodice.hexcolorgenerator.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.kodice.hexcolorgenerator.services.RandomColorsPack;
+import models.dto.RandomColorsResponse;
 
 @RestController
 public class HexColorGeneratorController {
@@ -15,9 +16,10 @@ public class HexColorGeneratorController {
 	@Autowired
 	private RandomColorsPack randomColorsPack;
 	
-	@RequestMapping("/{nColors}")
-	public List<String> hexColorGenetarot(@PathVariable int nColors) {
-		return this.randomColorsPack.colorsArray(nColors);
+	@GetMapping("/{nColors}")
+	public RandomColorsResponse hexColorGenetarot(@PathVariable int nColors) {
+		List<String> colors = this.randomColorsPack.colorsArray(nColors);
+		return new RandomColorsResponse(colors); 
 	}
 
 }

26f178ca203fa98cf81cf10ff378fb9d7a201d91
Efren Cabrera <efren@cabrera.dev>
2020-09-14 09:28:50 -0700

    Add seed and nColors as query parameters

--- src/main/java/dev/kodice/hexcolorgenerator/web/HexColorGeneratorController.java
+++ src/main/java/dev/kodice/hexcolorgenerator/web/HexColorGeneratorController.java
@@ -4,22 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.kodice.hexcolorgenerator.services.RandomColorsPack;
 import models.dto.RandomColorsResponse;
 
 @RestController
+@RequestMapping(path = "/random")
 public class HexColorGeneratorController {
-	
+
 	@Autowired
 	private RandomColorsPack randomColorsPack;
-	
-	@GetMapping("/{nColors}")
-	public RandomColorsResponse hexColorGenetarot(@PathVariable int nColors) {
-		List<String> colors = this.randomColorsPack.colorsArray(nColors);
-		return new RandomColorsResponse(colors); 
+
+	@GetMapping
+	public RandomColorsResponse defaultHexColor(@RequestParam(defaultValue = "1") int nColors,
+			@RequestParam(defaultValue = "28") int seed) {
+		List<String> colors = this.randomColorsPack.colorsArray(nColors, seed);
+		RandomColorsResponse response = new RandomColorsResponse(colors);
+		return response;
 	}
 
 }
